52666999ac181488e0011a77b98694632cd8288f
RockerKitten <84648053+RockerKitten@users.noreply.github.com>
2021-08-16 19:23:30 -0500

    initial setup

---
+++ src/War/Deck.java
@@ -0,0 +1,20 @@
+package War;
+
+import java.util.List;
+
+public class Deck extends Cards {
+    private static List<Cards>cards;
+
+    public Deck(int value, StringBuilder name) {
+        super(value, name);
+    }
+
+    public static void shuffle()
+    {
+
+    }
+    public static Cards draw()
+    {
+        return null;
+    }
+}
---
+++ src/War/Player.java
@@ -0,0 +1,25 @@
+package War;
+
+import java.util.List;
+
+public class Player {
+    private static List<Cards> hand;
+    private static int score;
+
+    public static void describe()
+    {
+
+    }
+    public static void flip()
+    {
+
+    }
+    public static void draw()
+    {
+
+    }
+    public static int incrementScore(int score)
+    {
+        return 0;
+    }
+}
